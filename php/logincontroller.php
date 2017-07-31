c63983dfde319a4181b13bd4a46d5743b57f4862
Geetswain <geetsswain@gmail.com>
2017-08-01 00:20:32 +0530

    f

--- php/logincontroller.php
+++ php/logincontroller.php
@@ -5,9 +5,9 @@
 		
 		$email=trim(mysqli_real_escape_string($conn,$_POST['email']));
 		$password=trim(mysqli_real_escape_string($conn,md5($_POST['password'])));
-		$query=mysqli_query($conn,"SELECT email,password FROM register_user WHERE email='$email' AND password='$password' ");
+		$query=mysqli_query($conn,"SELECT email,password FROM register_user WHERE email=$email AND password=$password ");
 
-		if(mysqli_num_rows($result)==1)
+		if(mysqli_num_rows($query)==1)
 			{echo "Success Log In";}
 		else
 			{echo "Error Log In";}
