342307b0a088d4ca98ae24daa1676138946cbed0
Crypto <kamakhya166@gmail.com>
2017-07-31 01:37:25 -0700

    asd

--- pp_upload_controller.php
+++ pp_upload_controller.php
@@ -13,7 +13,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES[$x]["_PP"], $target_file)) {
         echo "The file ". basename( $_FILES[$X]["_PP"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
