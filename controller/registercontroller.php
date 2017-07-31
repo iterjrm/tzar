446e980422565d44bb3c42dc1914e70d82c4b6bd
Geetswain <geetsswain@gmail.com>
2017-07-31 20:57:16 +0530

    w

--- controller/registercontroller.php
+++ controller/registercontroller.php
@@ -1,14 +1,10 @@
 <?php
 	if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) )
 	{	
+		include('../php/connect.php');
 		$name=trim(mysqli_real_escape_string($conn,$_POST['name']));
 		$email=trim(mysqli_real_escape_string($conn,$_POST['email']));
 		$password=trim(mysqli_real_escape_string(md5($conn,$_POST['password'])));
-		include('../php/connect.php');
-
-		echo $name;
-		exit();
-
 		$query=mysqli_query($conn,"INSERT INTO register_user(name,email,password) VALUES ('$name','$email','$password')");
 	}
 	else{
