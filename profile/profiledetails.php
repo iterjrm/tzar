31610d68a5797ed07afb0c24b362a3d4a7d587a0
$!d#@r+# $@m@n+@r@Y <sidharthsray@gmail.com>
2017-07-31 15:22:17 +0530

    ss

--- profile/profiledetails.php
+++ profile/profiledetails.php
@@ -433,8 +433,9 @@
 
                 <h2>Profile Details</h2>
                   <div class="row">
-                <div class="box-footer">
+               <!-- /.post -->
 
+                <!-- Post -->
                       <div class="col-md-6 col-sm-6 col-xs-12 "  >
                               <label for="fname" class="col-md-1 col-sm-1 col-xs-1"><span class="label label-info">First Name</span></label>
 
