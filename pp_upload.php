21a81cb7a0ce6801a2ba3f7daa542dd7c480104f
Crypto <kamakhya166@gmail.com>
2017-07-31 01:40:11 -0700

    asd

--- pp_upload.php
+++ pp_upload.php
@@ -1,12 +1,10 @@
-<?php
-?>
 <!DOCTYPE html>
 <html>
 <body>
 
 <form action="pp_upload_controller.php" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="file" name="pp>" id="pp"><br>
+    <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
