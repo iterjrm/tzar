e595ef1196a2a4952466b3b967746ba8e2495b09
Crypto <kamakhya166@gmail.com>
2017-07-31 02:20:51 -0700

    sad

--- php/connect.php
+++ php/connect.php
@@ -1,3 +1,10 @@
 <?php
 
-?>
+	$conn=new mysqli("localhost","jrm","sikujyoti753","training_user_jrm");
+
+	if ($conn->connect_error) 
+	{
+    	die("Connection failed: " . $conn->connect_error);
+	} 
+
+  ?>
