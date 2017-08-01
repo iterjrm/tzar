77742a59833c96315fe8bb95ec7297ea89d767ce
$!d#@r+# $@m@n+@r@Y <sidharthsray@gmail.com>
2017-08-01 16:54:44 +0530

    a

--- profile/header.php
+++ profile/header.php
@@ -60,8 +60,18 @@ if (isset($_SESSION['u_id'])) {
 
   <!-- Google Font -->
   <link rel="stylesheet" href="../https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
 <div class="wrapper">
 
   <header class="main-header">
