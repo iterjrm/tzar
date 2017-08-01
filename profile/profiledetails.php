91e24c1b4c13a0084da7aa29f056598dbc3eb3de
$!d#@r+# $@m@n+@r@Y <sidharthsray@gmail.com>
2017-08-01 16:36:24 +0530

    ss

--- profile/profiledetails.php
+++ profile/profiledetails.php
@@ -29,19 +29,19 @@ include("header.php");
                   <div class="row">
                     <form class="col s12">
                       <div class="row">
-                        <div class="input-field col s6">
+                        <div class="input-field col s4">
                           <i class="material-icons prefix">account_circle</i>
                           <input id="icon_prefix" type="text" class="validate">
                           <label for="icon_prefix">First Name</label>
                         </div>
 
-                        <div class="input-field col s6">
+                        <div class="input-field col s4">
                           <i class="material-icons prefix">account_circle</i>
                           <input id="icon_prefix" type="text" class="validate">
                           <label for="icon_prefix">Middle Name</label>
                         </div>
 
-                        <div class="input-field col s6">
+                        <div class="input-field col s4">
                           <i class="material-icons prefix">account_circle</i>
                           <input id="icon_prefix" type="text" class="validate">
                           <label for="icon_prefix">Last Name</label>
