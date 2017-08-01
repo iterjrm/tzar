6bba63be70a7c5443f7169c04adb432e1812a60d
Srinibas Biswal <srinibasbiswal12@gmail.com>
2017-08-01 17:21:28 +0530

    table

--- php/registercontroller.php
+++ php/registercontroller.php
@@ -5,7 +5,7 @@
 		$name=trim(mysqli_real_escape_string($conn,$_POST['fname']));
 		$email=trim(mysqli_real_escape_string($conn,$_POST['email']));
 		$password=trim(mysqli_real_escape_string($conn,md5($_POST['password'])));
-		$query=mysqli_query($conn,"INSERT INTO trainuserprof(name,email,password) VALUES ('$fname','$email','$password')");
+		$query=mysqli_query($conn,"INSERT INTO trainuserprof(fname,email,password) VALUES ('$name','$email','$password')");
 	}
 	else{
 		header("location:register.php");
