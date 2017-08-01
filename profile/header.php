c1555b6d44b965366dceb4b0d4e450575035f89f
Srinibas Biswal <srinibasbiswal12@gmail.com>
2017-08-01 15:53:50 +0530

    removd timer

--- profile/header.php
+++ profile/header.php
@@ -1,20 +1,3 @@
-<?php  
-
-$idletime=10;
-if (time()-$_SESSION['timestamp']>$idletime)
-{
-  session_unset();
-  session_destroy();  
-  echo "logged out";
-  header("Location: ../register/login")
-}
-else
-{
-  $_SESSION['timestamp']=time();
-}
-
-
-?>
 <head>
   <meta charset="utf-8"><!DOCTYPE html>
   <style>
