5686e54b0007023d66f4c33050714a11b218526e
Srinibas Biswal <srinibasbiswal12@gmail.com>
2017-08-01 19:03:52 +0530

    sjdbjs

--- register/login.php
+++ register/login.php
@@ -1,4 +1,62 @@
-<!DOCTYPE html>
+<?php
+    ob_start ( ) ;
+    session_start ( ) ;
+    $login_message = "" ;
+    if ( require '../php/connect.php' ) {
+        if ( isset ( $_SESSION [ 'email' ] ) === true ){
+            $login_message = "Seems You're already logged in " ;
+            header('Location: profile/index.php');  
+        } 
+        if ( ( isset ( $_POST [ 'email' ] )) && ( isset ( $_POST [ 'password' ] )) && !( isset ( $_SESSION [ 'email' ] )) ) {
+            $email =  $_POST [ 'email' ];
+            $password = $_POST [ 'password' ] ;
+           
+            if ( !( empty ( $email ) ) && !( empty ( $password ) )   ) {
+                $checkuserlogin = $conn -> prepare ( 'SELECT * FROM trainuserprof WHERE email = ? ;' ) ;
+                $checkuserlogin -> execute ( array ( $email) ) ;
+                $loginarray = $checkuserlogin -> fetch ( ) ;
+                    if ( $password == $loginarray [ 'password' ] ) {
+                        if ( ( $_SESSION [ 'email' ] = $loginarray [ 'email' ] ) && ( $_SESSION [ 'fname' ] = $loginarray [ 'fname' ] ) && ( $_SESSION [ 'email' ] = $loginarray [ 'email' ] ) ) {
+                            $login_message = "Logging You in..." ;
+                            
+                            header ( "Location: ../profile/index" ) ;
+
+                       
+                            
+                        }
+
+                        else $login_message = "Couldn't log You in" ;
+                    }
+
+                    elseif (( $_SESSION [ 'email' ] = $loginarray [ 'email' ] ) && $password != $loginarray [ 'password' ]) {
+                      $login_message = "Invalid Password" ;
+                      # code...
+                    }
+                    else {
+                        $login_message = "Either this Account Isn't Registered With Us or the Registration Has Been Disabled Due To Non Payment<br />" ;
+                    }
+            }
+            else $login_message = "Oops! Couldn't log you in. Invalid credentials. :(" ;
+        }
+    }
+    else $login_message = "Sorry! Internal error occurred" ;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <html >
 <head>
   <meta charset="UTF-8">
@@ -14,11 +72,20 @@
       <div class="registration">
         <h1>LOGIN</h1>
         
-        <form method="post" action="../php/logincontroller2.php">
+        <form method="post" action="">
           <input type="text" name="email" placeholder="Email Id" required="required"/>
           <input type="password" name="password" placeholder="Password" required="required"/>
           <br>
           <button class="btn btn-primary btn-block btn-large" type="submit">Let me in.</button>
+
+            
+   <?php
+                if ( ( isset ( $login_message ) === true ) && ( empty ( $login_message ) === false ) ) :
+            ?>
+            <strong><?php echo $login_message ; echo $loop; ?></strong>
+            <?php
+                endif ;
+            ?>
         </form>
       </div>
     </div>
