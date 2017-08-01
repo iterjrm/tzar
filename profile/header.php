130dad386febf9989061b9c3d796614c32e1b7fc
Crypto <kamakhya166@gmail.com>
2017-08-01 22:26:52 +0530

    sad

--- profile/header.php
+++ profile/header.php
@@ -78,7 +78,7 @@ if (isset($_SESSION['u_id'])) {
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b>LTE</span>
+      <span class="logo-lg"><b>TZAR</b>Aerospace</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
